0ede9aaff5faa535fdaa5f81479586d149e83a0f
Vijay Raghuwanshi <gvijay745@gmail.com>
2020-08-20 22:45:05 +0530

    Basic Structure of C Program

---
+++ Hello World.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    printf("Hello world this is my first c program.");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Hello this is my first full fledge C Language Tutorial in hindi
